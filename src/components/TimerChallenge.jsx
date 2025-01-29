6c178a824de1a1ed22d8db8a2c8f3c9be7560f4b
Pathfinder1613 <quin.strickland@outlook.com>
2025-01-28 22:15:16 -0600

    Forwarding Refs to custom components

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,23 +1,40 @@
+import React, { useState } from 'react';
+import ResultModal from './ResultModal';
+
 export default function TimerChallenge({title, targetTime}) { 
+    const [timerStarted, setTimerStarted] = useState(false);
+    const [timerExpired, setTimerExpired] = useState(false);
 
     function handleClick() {
-        console.log('Button clicked'); 
+        setTimeout(() => {
+            setTimerExpired(true) 
+            dialog.current.showModal()
+        }, targetTime * 1000);
+        setTimerStarted(true);
     }
-    return (<section id="challenge">
+
+    function handleStop() {
+        clearTimeout(timerExpired.current);
+    }
+
+
+    return (
+    <>
+    {timerExpired && <ResultModal ref={dialog} targetTime={targetTime} result="You Lost" />}
+    <section id="challenge">
         <h2>{title}</h2>
         <p className="challenge">
             {targetTime} seconds {targetTime > 1 ? 's' : ''} to go!
             
         </p>
         <p>
-            <button>
-                Start Challenge
-
+            <button onClick={handleClick}>
+                {timerStarted ? 'stop' : 'Start'} Challenge
             </button>
         </p>
-        <p className="" >
-            Time is running... / timer inactive
+        <p className={timerStarted ? 'active' : undefined} >
+            {timerStarted ? 'Time is running...' : 'timer inactive'}
         </p>?
-    </section>
+    </section></>
     );
 }
